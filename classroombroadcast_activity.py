a195ac7f079c1d3c0b8b02f9b4e2f9127f03b463
Rafael Ortiz <rafael@activitycentral.com>
2011-11-30 21:36:56 -0500

    removing unused imports

--- classroombroadcast_activity.py
+++ classroombroadcast_activity.py
@@ -1,13 +1,8 @@
-# Copyright (C) 2011, Ariel Calzada <ariel.calzada@gmail.com>.
-# Copyright (C) 2008, One Laptop Per Child
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
 #
 # This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
+
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
@@ -22,10 +17,7 @@ from sugar.activity.activity import ActivityToolbox
 import gtk
 import subprocess
 import os
-import signal
 import socket
-import fcntl
-import struct
 import commands
 
 
