295d8c5d168fcc7677ab71981903a835bd47ac4c
Ariel Calzada <ariel@activitycentral.com>
2012-01-24 09:54:38 -0500

    Update internationalization, divide code in new modules broadcast.py and utilities.py, removed telepathy related code

---
+++ broadcast.py
@@ -0,0 +1,154 @@
+from gettext import gettext as _
+import gtk
+import pango
+
+from utilities import Utilities
+
+class BroadcastProcess():
+    """Broadcast process component
+    """
+
+    # utilities
+    _utilities = None
+
+    # process details
+    _programName = "x11vnc"
+    _args = ["-viewonly", "-shared", "-bg", "-forever", "-solid", "-wireframe"]
+
+    def __init__(self):
+        """Constructor
+        """
+        self._utilities = Utilities()
+
+    def getStatus(self):
+        """get current status of the vnc process
+        """
+        st = self._utilities.checkProgramStatus(self._programName)
+
+        return st[0]
+
+    def changeStatus(self):
+        """change current status of the vnc proces
+        """
+        if self.getStatus():
+            self._utilities.endProgram(self._programName)
+        else:
+            self._utilities.startProgram(self._programName,self._args)
+
+    def getProcessInfo(self):
+        return self._utilities.getNetworkProcessInfo(self._programName)
+
+
+class BroadcastUI():
+    """Broadcast UI component for Classroom Broadcast Activity
+    """
+    # Constants
+    _greenColor = '#00E500'
+    _redColor = '#FF0000'
+
+    # I18N
+    _START = _('Start')
+    _STOP = _('Stop')
+
+    # UI elements
+    _box = None
+    _button = None
+    _label = None
+    _toolbar = None
+    _boxAlign = None
+
+    # activity, process
+    _activity = None
+    _process = None
+
+    def __init__(self, activity, process):
+        """Constructor
+        """
+        self._activity = activity
+        self._process = process
+
+    def loadUI(self):
+        """Create and show UI
+        """
+        # Box
+        self._box = gtk.VBox()
+
+        # Label
+        self._label = gtk.Label()
+
+        # Button
+        self._button = gtk.Button()
+        self._button.set_size_request(200, 50)
+        self._button.connect("clicked", self.buttonClicked)
+
+        # Add button to box
+        self._box.pack_start(self._button)
+
+        # Add label to box
+        self._box.pack_start(self._label, padding=20)
+
+        # Box Align (xalign, yalign, xscale, yscale)
+        self._boxAlign = gtk.Alignment(0.5, 0.5, 0, 0)
+        self._boxAlign.add(self._box)
+
+        # Set canvas with box alignment
+        self._activity.set_canvas(self._boxAlign)
+
+    def setButtonBG(self, color):
+        """Change button bg color
+        """
+        self._button.modify_bg(gtk.STATE_NORMAL, gtk.gdk.color_parse(color))
+
+    def setButtonLabel(self, txt):
+        """Change button label
+        """
+        self._button.set_label(txt)
+
+    def buttonClicked(self, widget, data=None):
+        """Button clicked event handler
+        """
+        self._process.changeStatus()
+        self.showStatus()
+
+    def setLabelTXT(self, txt):
+        """Change label text
+        """
+        self._label.set_label(txt)
+
+    def showStatus(self):
+        """Show VNC status
+        """
+        state = self._process.getStatus()
+
+        if not state:
+            self.setButtonBG(self._greenColor)
+            self.setButtonLabel(self._START)
+            self.setLabelTXT("")
+        else:
+            self.setButtonBG(self._redColor)
+            self.setButtonLabel(self._STOP)
+            self.setLabelTXT(self._process.getProcessInfo())
+
+class Broadcast():
+    """Broadcast component for Classroom Broadcast Activity
+    """
+    _activity = None
+    _process = None
+    _ui = None
+
+    def __init__(self,activity):
+        """Constructor
+        """
+        self._activity = activity
+        self._process = BroadcastProcess()
+        self._ui = BroadcastUI(self._activity, self._process)
+
+    def loadUI(self):
+        """Load UI
+        """
+        self._ui.loadUI()
+
+    def showStatus(self):
+        """Show broadcast status
+        """
+        self._ui.showStatus()
---
+++ utilities.py
@@ -0,0 +1,133 @@
+import subprocess
+import os
+import socket
+import commands
+
+class Utilities():
+    """Utilities
+    """
+    # I18N
+    _IPS = _('IPs')
+    _PROCESSID = _('Process ID')
+    _HOSTNAME = _('Hostname')
+
+    def __init__(self):
+        """Constructor
+        """
+        pass
+
+    def checkProgramStatus(self, programName):
+        """
+        Check if 'programName' is running
+        Returns: [ A, [ B ] ]
+        A: True | False if the program is running
+        B: list of PID
+        """
+        result = []
+
+        ps = subprocess.Popen(["pidof",programName],stdout=subprocess.PIPE)
+        pid = ps.communicate()[0].strip().split(" ")
+        ps.stdout.close()
+        pids = []
+
+        for p in pid:
+            p = p.strip()
+
+            if p != "":
+                pids.append(p)
+
+        if len(pids) > 0:
+            return [True, pids]
+
+        return [False, []]
+
+    def getHostname(self):
+        """Get server name
+        """
+        return socket.gethostname()
+
+    def getNetworkInterfaces(self):
+        """Get server network interfaces names
+        """
+        f = open('/proc/net/dev', 'r')
+        lines = f.readlines()
+        f.close()
+        lines.pop(0)
+        lines.pop(0)
+
+        interfaces = []
+        for line in lines:
+            interface = line.strip().split(" ")[0].split(":")[0].strip()
+            interfaces.append(interface)
+
+        return interfaces
+
+    def getNetworkIPs(self, interfaces):
+        """Get server IPs per interface
+        """
+        pattern = "inet addr:"
+        cmdName = "/sbin/ifconfig"
+        ips = {}
+
+        for interface in interfaces:
+            cmd = cmdName + " " + interface
+            output = commands.getoutput(cmd)
+            inet = output.find(pattern)
+
+            if inet >= 0:
+                start = inet + len(pattern)
+                end = output.find(" ", start)
+
+                ip = output[start:end]
+                ips[interface] = ip
+            else:
+                ips[interface] = ""
+
+        return ips
+
+    def getNetworkInfo(self):
+        """Get server network map {IFACE:IP}
+        """
+        info = ""
+
+        interfaces = self.getNetworkInterfaces()
+        ips = self.getNetworkIPs(interfaces)
+
+        for interface, ip in ips.iteritems():
+            if info != "":
+                info += "\n         "
+
+            info += interface + ": " + ip
+
+        return info
+
+    def endProgram(self, programName):
+        status = self.checkProgramStatus(programName)
+
+        if status [ 0 ] == True:
+            pids = status[1]
+            for pid in pids:
+                os.system("kill -9 " + pid)
+
+    def startProgram(self, programName, args=[]):
+        cmd = [programName]
+
+        if len(args) > 0:
+            for arg in args:
+                cmd.append(arg)
+
+        subprocess.call(cmd, shell=False)
+
+    def getNetworkProcessInfo(self,programName):
+        status = self.checkProgramStatus(programName)
+        pid = ""
+        if status [ 0 ]:
+            pid = ",".join(status[1])
+
+        txt = self._PROCESSID + " = " + pid
+        txt += "\n"
+        txt += self._HOSTNAME + " = " + self.getHostname()
+        txt += "\n"
+        txt += self._IPS + " = " + self.getNetworkInfo()
+
+        return txt
